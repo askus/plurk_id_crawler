54fb7c00d8772890dd3066c3dabc218f846b9c66
askus <b2016.tw@gmail.com>
2012-09-08 16:16:09 +0800

    test

--- crowd.py
+++ crowd.py
@@ -29,10 +29,7 @@ for l in infile:
 	tmp = l.strip().split("\t")
 	userid = tmp[0]
 
-	try:
-		temp = plurk.callAPI("/APP/Profile/getPublicProfile", {"user_id": userid})
-	except:
-		continue
+	temp = plurk.callAPI("/APP/Profile/getPublicProfile", {"user_id": userid})
 	
 	print "[%s][%.2f] %d/%d" %( filename_prefix  , ( float(i)/ float(total_length) ) , i, total_length )  
 	i +=1 
